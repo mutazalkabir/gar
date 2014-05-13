47044141065f0a43f4bac749aaf34fe91da8e369
tuncak30 <tuncak30@gmail.com>
2014-05-13 21:35:27 +0300

    Haydi bismillah

---
+++ js/data_connector.js
@@ -0,0 +1,57 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: tuncakin
+ * Date: 13/05/14
+ * Time: 21:30
+ * To change this template use File | Settings | File Templates.
+ */
+
+/* Login - Register */
+function register(_email,_name,_surname,_pass,_gender,_bDate)
+{
+
+    $.ajax({
+        url: "register.php",
+        type: "POST",
+        data:{ mail: _email, name:_name, surname:_surname,pass:_pass,gender:_gender,bdate:_bDate },
+        dataType:"json",
+        cache: false,
+        xhrFields: {
+            withCredentials: true
+        }
+    }).done(function(data) {
+
+            // alert( "success" +data );
+        }).fail(function(data) {
+
+            // alert( "error" +data );
+        }).always(function(data) {
+
+            // alert( "finished" + data);
+        });
+}
+
+function login(_email,_pass)
+{
+
+    $.ajax({
+        url: "login.php",
+        type: "POST",
+        data:{ mail: _email,pass:_pass},
+        dataType:"json",
+        cache: false,
+        xhrFields: {
+            withCredentials: true
+        }
+    }).done(function(data) {
+
+            // alert( "success" +data );
+        }).fail(function(data) {
+
+            // alert( "error" +data );
+        }).always(function(data) {
+
+            // alert( "finished" + data);
+        });
+}
+/* Login - Register */
