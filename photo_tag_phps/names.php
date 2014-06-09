023455184819904cdaa1856fb09aae9b64ea22bf
tuncak30 <tuncak30@gmail.com>
2014-06-09 20:12:47 +0300

    default profile picture

--- photo_tag_phps/names.php
+++ photo_tag_phps/names.php
@@ -43,5 +43,270 @@
     "id": 9,
     "label": "Bluz",
     "value": "Bluz"
+    },
+    {
+    "id": 10,
+    "label": "Bot",
+    "value": "Bot"
+    },
+    {
+    "id": 11,
+    "label": "Boxer Şort",
+    "value": "Boxer Şort"
+    },
+    {
+    "id": 12,
+    "label": "Çanta",
+    "value": "Çanta"
+    },
+    {
+    "id": 13,
+    "label": "Ceket",
+    "value": "Ceket"
+    },
+    {
+    "id": 14,
+    "label": "Çizme",
+    "value": "Çizme"
+    },
+    {
+    "id": 15,
+    "label": "Çorap",
+    "value": "Çorap"
+    },
+    {
+    "id": 16,
+    "label": "El Çantası",
+    "value": "El Çantası"
+    },
+    {
+    "id": 17,
+    "label": "Elbise",
+    "value": "Elbise"
+    },
+    {
+    "id": 18,
+    "label": "Eldiven",
+    "value": "Eldiven"
+    },
+    {
+    "id": 19,
+    "label": "Eşarp",
+    "value": "Eşarp"
+    },
+    {
+    "id": 20,
+    "label": "Eşofman",
+    "value": "Eşofman"
+    },
+    {
+    "id": 21,
+    "label": "Etek",
+    "value": "Etek"
+    },
+    {
+    "id": 22,
+    "label": "Gecelik",
+    "value": "Gecelik"
+    },
+    {
+    "id": 23,
+    "label": "Gömlek",
+    "value": "Gömlek"
+    } ,
+    {
+    "id": 24,
+    "label": "Gözlük",
+    "value": "Gözlük"
+    },
+    {
+    "id": 25,
+    "label": "Güneş Gözlüğü",
+    "value": "Güneş Gözlüğü"
+    },
+    {
+    "id": 26,
+    "label": "Hırka",
+    "value": "Hırka"
+    },
+    {
+    "id": 27,
+    "label": "Kazak",
+    "value": "Kazak"
+    },
+    {
+    "id": 28,
+    "label": "Kemer",
+    "value": "Kemer"
+    },
+    {
+    "id": 29,
+    "label": "Kol Saati",
+    "value": "Kol Saati"
+    },
+    {
+    "id": 30,
+    "label": "Kolye",
+    "value": "Kolye"
+    },
+    {
+    "id": 31,
+    "label": "Kot Pantalon",
+    "value": "Kot Pantalon"
+    },
+    {
+    "id": 32,
+    "label": "Kravat",
+    "value": "Kravat"
+    },
+    {
+    "id": 33,
+    "label": "Küpe",
+    "value": "Küpe"
+    },
+    {
+    "id": 34,
+    "label": "Kürk",
+    "value": "Kürk"
+    },
+    {
+    "id": 35,
+    "label": "Mayo",
+    "value": "Mayo"
+    },
+    {
+    "id": 36,
+    "label": "Mont",
+    "value": "Mont"
+    },
+    {
+    "id": 37,
+    "label": "Mücevher",
+    "value": "Mücevher"
+    },
+    {
+    "id": 38,
+    "label": "Palto",
+    "value": "Palto"
+    },
+    {
+    "id": 39,
+    "label": "Pantalon",
+    "value": "Pantalon"
+    },
+    {
+    "id": 40,
+    "label": "Papyon",
+    "value": "Papyon"
+    },
+    {
+    "id": 41,
+    "label": "Pelerin",
+    "value": "Pelerin"
+    },
+    {
+    "id": 42,
+    "label": "Pijama",
+    "value": "Pijama"
+    },
+    {
+    "id": 43,
+    "label": "Plaj Çantası",
+    "value": "Plaj Çantası"
+    },
+    {
+    "id": 44,
+    "label": "Sabahlık",
+    "value": "Sabahlık"
+    },
+    {
+    "id": 45,
+    "label": "Şal",
+    "value": "Şal"
+    },
+    {
+    "id": 46,
+    "label": "Şalvar",
+    "value": "Şalvar"
+    },
+    {
+    "id": 47,
+    "label": "Sandalet",
+    "value": "Sandalet"
+    },
+    {
+    "id": 48,
+    "label": "Şapka",
+    "value": "Şapka"
+    },
+    {
+    "id": 49,
+    "label": "Şort",
+    "value": "Şort"
+    },
+    {
+    "id": 50,
+    "label": "Spor Ayakkabısı",
+    "value": "Spor Ayakkabısı"
+    },
+    {
+    "id": 51,
+    "label": "Spor Çantası",
+    "value": "Spor Çantası"
+    },
+    {
+    "id": 52,
+    "label": "Süveter",
+    "value": "Süveter"
+    },
+    {
+    "id": 53,
+    "label": "Takım Elbise",
+    "value": "Takım Elbise"
+    },
+    {
+    "id": 54,
+    "label": "Tayt",
+    "value": "Tayt"
+    },
+    {
+    "id": 55,
+    "label": "Terlik",
+    "value": "Terlik"
+    },
+    {
+    "id": 56,
+    "label": "Topuklu Ayakkabı",
+    "value": "Topuklu Ayakkabı"
+    },
+    {
+    "id": 57,
+    "label": "T-Shirt",
+    "value": "T-Shirt"
+    },
+    {
+    "id": 58,
+    "label": "Tulum",
+    "value": "Tulum"
+    },
+    {
+    "id": 59,
+    "label": "Yağmurluk",
+    "value": "Yağmurluk"
+    },
+    {
+    "id": 60,
+    "label": "Yelek",
+    "value": "Yelek"
+    },
+    {
+    "id": 61,
+    "label": "Yüzük",
+    "value": "Yüzük"
+    },
+    {
+    "id": 62,
+    "label": "Diğer",
+    "value": "Diğer"
     }
 ]
